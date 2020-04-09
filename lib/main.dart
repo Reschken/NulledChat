1d1fd9f398f7df65ec3940056974563afc56c2ab
Muckie <luca@reschke.me>
2020-04-09 18:45:20 +0200

    Added: Saving Token to Memonry and using

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,7 @@ class MainApp extends StatefulWidget {
 
 class _scrollBehavior extends ScrollBehavior {
   @override
-  Widget buildViewportChrome(
-      BuildContext context, Widget child, AxisDirection axisDirection) {
+  Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
   }
 }
@@ -119,15 +118,12 @@ class _State extends State<MainApp> {
     groupsList.add(new Group(name: "Aqua", id: 91, color: Colors.indigo));
     groupsList.add(new Group(name: "Aqua", id: 90, color: Colors.lightGreen));
     groupsList.add(new Group(name: "Mod", id: 9, color: Colors.teal));
-    groupsList
-        .add(new Group(name: "Legendary", id: 38, color: Colors.amberAccent));
-    groupsList
-        .add(new Group(name: "Royal", id: 12, color: Colors.lightBlueAccent));
+    groupsList.add(new Group(name: "Legendary", id: 38, color: Colors.amberAccent));
+    groupsList.add(new Group(name: "Royal", id: 12, color: Colors.lightBlueAccent));
     groupsList.add(new Group(name: "Nova", id: 92, color: Colors.deepOrange));
     groupsList.add(new Group(name: "User", id: 3, color: Colors.grey));
     // TODO Add Rainbow effect
-    groupsList
-        .add(new Group(name: "Heavenly", id: 104, color: Colors.purpleAccent));
+    groupsList.add(new Group(name: "Heavenly", id: 104, color: Colors.purpleAccent));
     groupsList.add(new Group(name: "Vip", id: 7, color: Colors.pinkAccent));
     groupsList.add(new Group(name: "Moderator", id: 6, color: Colors.teal));
   }
@@ -177,7 +173,7 @@ class _State extends State<MainApp> {
     socket.emit("emojis", [
       {"all": "all"}
     ]);
-    _tryAuthenticate();
+    //_tryAuthenticate();
   }
 
   _getDeleted(dynamic data) {
@@ -206,7 +202,8 @@ class _State extends State<MainApp> {
 
   _setAuthenticate(String token) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString(token, token);
+    prefs.setString("token", token);
+    print(prefs.getString("token"));
     _tryAuthenticate();
   }
 
@@ -222,8 +219,7 @@ class _State extends State<MainApp> {
   _getAuthenticated(dynamic data) {
     SelfuserModel user = SelfuserModel.fromJson(data);
     setState(() {
-      selfUser = new SelfUser(
-          username: user.data.user.username, group: user.data.user.group);
+      selfUser = new SelfUser(username: user.data.user.username, group: user.data.user.group);
       isAuthenticated = true;
     });
   }
@@ -231,8 +227,7 @@ class _State extends State<MainApp> {
   _getEmoji(dynamic data) {
     EmojiModel model = EmojiModel.fromJson(data);
     for (int i = 0; i < model.emojis.length; i++) {
-      emojisList.add(
-          new Emoji(file: model.emojis[i].image, typed: model.emojis[i].typed));
+      emojisList.add(new Emoji(file: model.emojis[i].image, typed: model.emojis[i].typed));
     }
     socket.emit("subscribe", [
       {"channelName": "general"}
@@ -259,20 +254,13 @@ class _State extends State<MainApp> {
 
     if (isChatMessage) {
       ChatMessage newmsg = new ChatMessage(
-          txt: txt,
-          username: username,
-          group: group,
-          id: id,
-          isDeleted: false,
-          styled: styled);
+          txt: txt, username: username, group: group, id: id, isDeleted: false, styled: styled);
       setState(() {
         messagesList.add(newmsg);
       });
       _getContent(newmsg);
-      _scrollController.animateTo(
-          _scrollController.position.maxScrollExtent + 80,
-          duration: Duration(milliseconds: 500),
-          curve: Curves.easeOut);
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent + 80,
+          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
     } else {
       print("Got non-user message.");
     }
@@ -293,8 +281,8 @@ class _State extends State<MainApp> {
   _mentionUser(String user) {
     print(user);
     messageInput.text += "@" + user;
-    messageInput.selection = TextSelection.fromPosition(
-        TextPosition(offset: messageInput.text.length));
+    messageInput.selection =
+        TextSelection.fromPosition(TextPosition(offset: messageInput.text.length));
   }
 
   List<Emoji> emojisInMsg;
@@ -321,8 +309,7 @@ class _State extends State<MainApp> {
             }
           }
           // Add current Emoji to list with Emojis in this message
-          emojisInMsg.add(
-              new Emoji(typed: typed, file: matches.elementAt(i).group(0)));
+          emojisInMsg.add(new Emoji(typed: typed, file: matches.elementAt(i).group(0)));
         }
 
         // For every Emoji in Message
@@ -334,8 +321,7 @@ class _State extends State<MainApp> {
           String before;
           String middle = image;
           String behind;
-          if (message.txt.endsWith(e.typed.toLowerCase()) ||
-              message.txt.endsWith(e.typed)) {
+          if (message.txt.endsWith(e.typed.toLowerCase()) || message.txt.endsWith(e.typed)) {
             before = chunk[0];
             behind = "";
           } else if (message.txt.startsWith(e.typed)) {
@@ -345,8 +331,7 @@ class _State extends State<MainApp> {
             before = chunk[0];
             behind = chunk[1];
           }
-          chunks
-              .add(new MSGChunk(before: before, image: middle, behind: behind));
+          chunks.add(new MSGChunk(before: before, image: middle, behind: behind));
         }
         for (int j = 0; j < chunks.length; j++) {
           String before = chunks[j].before;
@@ -356,19 +341,16 @@ class _State extends State<MainApp> {
           contentsList.add(new ChatMessageContent(
               before: TextSpan(text: before),
               behind: TextSpan(text: behind),
-              image: ImageSpan(AssetImage(image),
-                  imageHeight: 30, imageWidth: 30)));
+              image: ImageSpan(AssetImage(image), imageHeight: 30, imageWidth: 30)));
         }
       } else {
-        contentsList
-            .add(new ChatMessageContent(before: TextSpan(text: message.txt)));
+        contentsList.add(new ChatMessageContent(before: TextSpan(text: message.txt)));
       }
     }
   }
 
   _getImageDialog(BuildContext context, ChatMessage message) {
-    RegExp exp =
-        new RegExp(r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+");
+    RegExp exp = new RegExp(r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+");
     var matches = exp.allMatches(message.txt);
     print(matches.elementAt(0).group(0).toString());
     if (matches.length != 0) {
@@ -452,8 +434,7 @@ class _State extends State<MainApp> {
                       top: 5.0),
                   child: new TextField(
                     controller: messageInput,
-                    decoration: new InputDecoration.collapsed(
-                        hintText: 'Send a message'),
+                    decoration: new InputDecoration.collapsed(hintText: 'Send a message'),
                   ),
                 )),
             Expanded(
@@ -465,9 +446,7 @@ class _State extends State<MainApp> {
                       right: .5,
                       top: .5),
                   child: new FlatButton.icon(
-                      onPressed: _sendMessage,
-                      icon: Icon(Icons.send),
-                      label: new Text('')),
+                      onPressed: _sendMessage, icon: Icon(Icons.send), label: new Text('')),
                 ))
           ],
         ),
@@ -487,8 +466,8 @@ class _State extends State<MainApp> {
           ListTile(
             title: Text("Login"),
             onTap: () async {
-              final result = await Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Login()));
+              final result =
+                  await Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
               _setAuthenticate(result);
             },
           )
@@ -521,22 +500,18 @@ class _State extends State<MainApp> {
         },
         child: Container(
           decoration: new BoxDecoration(
-              color: message.isDeleted
-                  ? Colors.red[400]
-                  : isDark ? Colors.black45 : Colors.white,
+              color: message.isDeleted ? Colors.red[400] : isDark ? Colors.black45 : Colors.white,
               border: new Border.all(color: groupColor),
               borderRadius: new BorderRadius.circular(10.0)),
           margin: new EdgeInsets.all(3.0),
-          padding: new EdgeInsets.only(
-              top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
+          padding: new EdgeInsets.only(top: 16.0, bottom: 16.0, right: 8.0, left: 8.0),
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               new Text(
                 message.username,
-                style: new TextStyle(
-                    color: message.isDeleted ? Colors.white : groupColor),
+                style: new TextStyle(color: message.isDeleted ? Colors.white : groupColor),
               ),
               new Container(
                 padding: EdgeInsets.all(2.0),
@@ -546,8 +521,7 @@ class _State extends State<MainApp> {
                         contentsList.last.image,
                         contentsList.last.behind
                       ]))
-                    : ExtendedText.rich(TextSpan(
-                        children: <InlineSpan>[contentsList.last.before])),
+                    : ExtendedText.rich(TextSpan(children: <InlineSpan>[contentsList.last.before])),
               )
             ],
           ),
